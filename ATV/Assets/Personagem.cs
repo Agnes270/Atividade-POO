24fbcbf4eb35a3c0cc9782516327274f6a21c1df
Agnes270 <carvalho.cecilia@escolar.ifrn.edu.br>
2025-06-10 16:21:05 -0300

    cmc

---
+++ ATV/Assets/Personagem.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+public class Personagem : MonoBehaviour
+{
+    [ SerializeField ]
+    private string nome;
+    [ SerializeField ]
+    private int dano;
+    [ SerializeField ]
+    private int saude ;
+    [ SerializeField ]
+    private float ataque;
+    [ SerializeField ]
+  
+
+    public void AtribuirNome(string nome)
+    {
+        this.nome = nome;
+    }
+
+    public string Nome()
+    {
+        return this.nome;
+    }
+
+    public void AtribuirDano(int dano)
+    {
+        this.dano = dano;
+    }
+
+    public int Dano()
+    {
+        return this.dano;
+    }
+    
+    
+    public void AtribuirSaude(int saude)
+    {
+        this.saude = saude;
+    }
+
+    public int Saude()
+    {
+        return this.saude;
+    }
+    
+    public void AtribuirAtaque(float ataque)
+    {
+        this.ataque = ataque;
+    }
+
+    public float Velocidade()
+    {
+        return this.ataque;
+    }
+   
+    
+
+
+   
+
+
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ ATV/Assets/Templario.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Templario : Personagem
+{
+    public enum ArmaDoTemplario
+    {
+        ESPADA, MACHADO, ADAGA
+    }
